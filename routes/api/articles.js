abc05c232d33e15f6a811bd13e2c576fc06221d8
Gustavo Gibo <gustavo.gibo@gmail.com>
2018-06-26 14:45:41 -0700

    pushing my back-end content so far

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -3,14 +3,6 @@ const articlesController = require("../../controllers/articlesController");
 
 // Matches with "/api/books"
 router.route("/")
-  .get(booksController.findAll)
-  .post(booksController.create);
-
-// Matches with "/api/books/:id"
-router
-  .route("/:id")
-  .get(booksController.findById)
-  .put(booksController.update)
-  .delete(booksController.remove);
-
+  .get(articlesController.getArticles);
+  
 module.exports = router;
