2a79e69d3d9f257bd25b12e18af8ca97924b84cb
Mridula Udaya Shankar <mridu.sh92@gmail.com>
2018-06-26 13:24:03 -0700

    Install packages for react and node to run and scripts to start servers

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.use(routes);
+require("./routes/api/articles.js")(app)
+// app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
