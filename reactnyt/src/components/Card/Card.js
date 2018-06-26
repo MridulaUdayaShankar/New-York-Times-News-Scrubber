84f20ec0b6e2f2f24b47d08b3994c1dcaf9794d0
Mridula Udaya Shankar <mridu.sh92@gmail.com>
2018-06-26 14:41:15 -0700

    Add card component

---
+++ reactnyt/src/components/Card/Card.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+const Card = () => (
+    <div class="card">
+        <div class="card-header">
+
+        </div>
+        <div class="card-body">
+            <h5 class="card-title"></h5>
+            <p class="card-text"></p>
+           <button type='button' class="btn btn-primary"></button>
+        </div>
+    </div>
+);
+
+
+export default Card;
