6441e931435602706a210bc7ea558d5d83f21945
Mridula Udaya Shankar <mridu.sh92@gmail.com>
2018-06-27 10:55:47 -0700

    Add components to Saved articles page

---
+++ reactnyt/src/pages/Saved/Saved.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { FormBtn as DeleteBtn } from "../../components/Form";
+import API from "../../utils/API.js";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../../components/Grid";
+import { Card, CardBody } from "../../components/Card";
+import "./Saved.css";
+
+class Saved extends Component {
+    state = {
+        articles: [],
+    };
+
+    componentDidMount() {
+        this.saveArticle();
+      }
+    deleteArticle = id => {
+        API.deleteArticle(id)
+            .then(res => this.loadArticles())
+            .catch(err => console.log(err));
+    };
+
+    render() {
+        return (
+            <Container fluid>
+                <Row>
+                    <Col size="md-6 sm-12">
+                        <h1>Saved Articles</h1>
+                        {this.state.articles.length ? (
+                            <Card>
+                                {this.state.articles.map(article => (
+                                    <CardBody key={article._id}>
+                                        <Link to={"/saved/:id" + article._id}>
+
+                                        </Link>
+                                        <DeleteBtn onClick={() => this.deleteArticle(article._id)} children='Delete' />
+                                    </CardBody>
+                                ))}
+                            </Card>
+                        ) : (
+                                <h3>No Results to Display</h3>
+                            )}
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
+}
