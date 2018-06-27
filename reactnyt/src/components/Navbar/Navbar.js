af179ee4d7c75a725f18340689ea556905aa9ae0
Mridula Udaya Shankar <mridu.sh92@gmail.com>
2018-06-27 00:14:39 -0700

    Fix misc styling

--- reactnyt/src/components/Navbar/Navbar.js
+++ reactnyt/src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = () => (
-    <nav className="navbar navbar-expand-lg" style= {{ "background-color": "rgb(140, 175, 140)" }}>
+    <nav className="navbar navbar-expand-lg">
         <a className="navbar-brand mb-0 h1" href="/">
             Saved Articles
         </a>
